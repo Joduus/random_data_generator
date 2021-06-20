8a3183f141c012be7df1c6be74d0d37b3832aca0
Joshua Duscha <joshua+git@duscha.dev>
2021-06-20 16:12:49 +0200

    Init

---
+++ Random data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Random_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
