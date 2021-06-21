6ec21088f568554994d56931b997638418c3275a
Joshua Duscha <joshua+git@duscha.dev>
2021-06-21 19:51:40 +0200

    Add random data generator

--- Random data/Program.cs
+++ Random data/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Random_data
 {
@@ -6,7 +7,21 @@ namespace Random_data
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string fileName = "randomdata.dat";
+            var fileSize = (long)Math.Pow(2, 30) * 5; // 5 GiB
+            
+            using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                var bytesLeft = fileSize;
+                var singleWriteOperationSize = 1024;
+                Byte[] buffer = new byte[singleWriteOperationSize];
+                var random = new Random();
+                for (; 0 < bytesLeft; bytesLeft -= singleWriteOperationSize)
+                {
+                    random.NextBytes(buffer);
+                    binaryWriter.Write(buffer);
+                }
+            }
         }
     }
 }
